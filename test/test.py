5b8082a12bc2a72ab8b19d5f1b2d54d973888b83
Matheus <matheusgabriel4454@gmail.com>
2026-04-20 18:04:35 -0300

    update

--- test/test.py
+++ test/test.py
@@ -1,3 +1,5 @@
+import pytest
+
 from src.main import *
 from unittest.mock import patch
 
@@ -8,11 +10,12 @@ def test_read_root():
 
 def test_read_item():
     result = read_item(1, None)
-    assert result == {"item_id": 1, "q": None}
+    assert result == {1, None}
 
 
-def test_funcaoteste():
+@pytest.mark.asyncio
+async def test_funcaoteste():
     with patch('random.randint', return_value=12345):
-        result = funcaoteste()
+        result = await funcaoteste()
 
     assert result == {"teste": True, "numero_aleatorio": 12345}
