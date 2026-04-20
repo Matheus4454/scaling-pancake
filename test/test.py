0b034358297d4e39f3026c93a739f364be377165
Matheus <matheusgabriel4454@gmail.com>
2026-04-20 17:53:55 -0300

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,22 +1,18 @@
 from src.main import *
 from unittest.mock import patch
 
-
 def test_read_root():
     result = read_root()
-    yield  result
     assert result == {"message": "Olá Mundo!"}
 
 
-def test_read_item(item_id: int, q: str | None = None):
-    result = read_item()
-    yield result
-    assert result == {"item_id": item_id, "q": q}
+def test_read_item():
+    result = read_item(1, None)
+    assert result == {"item_id": 1, "q": None}
 
-# 127.0.0.1:8000/teste1
+
 def test_funcaoteste():
     with patch('random.randint', return_value=12345):
         result = funcaoteste()
-        yield result
 
     assert result == {"teste": True, "numero_aleatorio": 12345}
